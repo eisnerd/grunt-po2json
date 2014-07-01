2602c37e1d4611fc0c4443cec002a15e68a56e2f
David Eisner <david.eisner@oriel.oxon.org>
2014-07-01 14:24:25 +0100

    Multidomain and compression output format options

--- tasks/po2json.js
+++ tasks/po2json.js
@@ -20,25 +20,57 @@ module.exports = function(grunt) {
       nodeJs: false,
       requireJs: false,
       singleFile: false,
-      stringOnly: false
+      stringOnly: false,
+      multiDomain: true,
+      compress: false,
+      parseFilename: true
     });
+
+    var _options = options;
+    if (options.multiDomain)
+      options.singleFile = true;
 
     var path = require('path');
     var po2json = require('po2json');
+    var _ = require('underscore');
 
     this.files.forEach(function(line) {
       var dest, extension, out = {};
       line.src.forEach(function(file) {
-        var data = po2json.parseFileSync(file, options);
         var filename = path.basename(file, (path.extname(file)));
+        var options = _.clone(_options);
+        var lang = undefined;
+        if (options.parseFilename)
+        {
+          var m = /([^/]*)[/]LC_MESSAGES[/](.*)\.[^.]*$/.exec(file);
+          if (m)
+          {
+            lang = filename = m[1];
+            options.domain = m[2];
+          }
+        }
+        var data = po2json.parseFileSync(file, options);
+        try
+        {
+          if (lang && !data.locale_data[options.domain][''].lang)
+            data.locale_data[options.domain][''].lang = lang;
+        }
+        catch(e)
+        {}
         if (!options.singleFile)
         {
           extension = (options.nodeJs || options.requireJs ? 'js' : 'json');
           dest = path.join(line.dest, filename + '.' + extension);
           writeObj(data, dest, options);
         }
+        else if (options.multiDomain)
+        {
+          out[filename] = _.extend(out[filename] || {}, data.locale_data);
+        }
         else
+        {
           out[filename] = data;
+        }
       });
       if (options.singleFile)
       {
@@ -60,11 +92,27 @@ module.exports = function(grunt) {
       data = nullArrayToString(data);
     var contents = JSON.stringify(data);
     if (options.nodeJs) {
-      contents = "module.exports = " + contents + ";";
+      if (options.compress) {
+        var lz = require('lz-string');
+        contents = "module.exports = JSON.parse(require('lz-string').decompress(" +
+                        JSON.stringify(lz.compress(contents)) +
+                    "));";
+      } else {
+        contents = "module.exports = " + contents + ";";
+      }
     } else if (options.requireJs) {
-      contents =  "define(function() {\n" +
-                  "    return " + contents + ";\n" +
-                  "});\n";
+      if (options.compress) {
+        var lz = require('lz-string');
+        contents = "define(['lz-string'], function(lz) {\n" +
+                   "    return JSON.parse(lz.decompressFromUTF16(" +
+                            JSON.stringify(lz.compressToUTF16(contents)) +
+                        "));\n" +
+                   "});\n";
+      } else {
+        contents =  "define(function() {\n" +
+                    "    return " + contents + ";\n" +
+                    "});\n";
+      }
     }
     grunt.file.write(dest, contents);
     grunt.log.writeln('File "' + dest + '" created.');
